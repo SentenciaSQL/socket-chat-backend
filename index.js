c8b17e11b36632eb65a585ff1d8341d3d74b5b63
SentenciaSQL <inf512.andres.frias@gmail.com>
2021-07-06 16:55:28 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const { dbConnection } = require('./database/config');
+dbConnection();
+
+const app = express();
+
+app.use(express.json());
+
+// Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+require('./sockets/socket');
+
+const publicPath = path.resolve( __dirname, 'public' );
+
+app.use( express.static( publicPath) );
+
+app.use('/api/login', require('./routes/auth'));
+
+server.listen( process.env.PORT, (err) => {
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto!', process.env.PORT);
+});
