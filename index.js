9618684f9961b37122d882088ce75a4424a6b92c
SentenciaSQL <inf512.andres.frias@gmail.com>
2021-07-08 19:14:23 -0400

    final

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath) );
 
 app.use('/api/login', require('./routes/auth'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/mensajes', require('./routes/mensajes'));
 
 server.listen( process.env.PORT, (err) => {
     if ( err ) throw new Error(err);
